649a756a0a3ae0de684d503cc076ef4707ebe32a
ricardocastellr <rcastell@ucol.mx>
2024-04-14 16:45:39 -0600

    First commit

---
+++ src/components/MovieDetalis.jsx
@@ -0,0 +1,12 @@
+import MovieDetails from './MovieDetails';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Buscar Detalles de Película</h1>
+            <MovieDetails />
+        </div>
+    );
+};
+
+export default App;
